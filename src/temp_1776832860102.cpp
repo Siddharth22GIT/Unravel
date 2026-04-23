095db2bdda2d3e866597b1edfee19ca155518126
Divyansh <divyanshchandrakar2005@gmail.com>
2026-04-23 14:30:34 +0530

    added test cases and problem description in the grind

---
+++ src/temp_1776832860102.cpp
@@ -0,0 +1,13 @@
+// Paste your code here to analyze its complexity
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << i * j << "\n";
+        }
+    }
+    return 0;
+}
